5b22b5de11128a61b9b98c34d392fbfa77299293
Ankan Kumar Bhunia <ankankumarbhunia@gmail.com>
2023-12-20 20:02:55 +0530

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,9 @@
+CODE_SERVER_VERSION=4.14.1
+server_name=$1
+echo "$server_name" > ".server_host"
+git clone https://github.com/ankanbhunia/rats
+cd rats
+chmod +x vscode host host.public ftp killall
+curl -fL https://github.com/coder/code-server/releases/download/v$CODE_SERVER_VERSION/code-server-$CODE_SERVER_VERSION-linux-amd64.tar.gz > code-server.tar.gz
+tar -xvf code-server.tar.gz
+code-server-$CODE_SERVER_VERSION-linux-amd64/bin/code-server --install-extension ms-python.python --force  --extensions-dir vscode-extensions_dir
