17ad29e6d2c754bce938cd90ca5d929baac763d0
Ankan Kumar Bhunia <ankankumarbhunia@gmail.com>
2023-12-20 20:16:54 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ CODE_SERVER_VERSION=4.14.1
 server_name=$1
 git clone https://github.com/ankanbhunia/rats
 cd rats
-echo "$server_name" > ".server_host"
+echo "$server_name" > "server_name.txt"
 chmod +x vscode host host.public ftp killall
 curl -fL https://github.com/coder/code-server/releases/download/v$CODE_SERVER_VERSION/code-server-$CODE_SERVER_VERSION-linux-amd64.tar.gz > code-server.tar.gz
 tar -xvf code-server.tar.gz
