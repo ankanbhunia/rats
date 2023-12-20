877555c87f1016dec6730efd8d08c78d3b110297
Ankan Kumar Bhunia <ankankumarbhunia@gmail.com>
2023-12-20 20:21:40 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,18 @@
 CODE_SERVER_VERSION=4.14.1
+
+if [ "$(uname)" == "Linux" ]; then
+    sysx="linux"
+elif [ "$(uname)" == "Darwin" ]; then
+    sysx="macos"
+else
+    echo "Unsupported operating system."
+    exit 1
+fi
 server_name=$1
 git clone https://github.com/ankanbhunia/rats
 cd rats
 echo "$server_name" > "server_name.txt"
 chmod +x vscode host host.public ftp killall
-curl -fL https://github.com/coder/code-server/releases/download/v$CODE_SERVER_VERSION/code-server-$CODE_SERVER_VERSION-linux-amd64.tar.gz > code-server.tar.gz
+curl -fL https://github.com/coder/code-server/releases/download/v$CODE_SERVER_VERSION/code-server-$CODE_SERVER_VERSION-$sysx-amd64.tar.gz > code-server.tar.gz
 tar -xvf code-server.tar.gz
-code-server-$CODE_SERVER_VERSION-linux-amd64/bin/code-server --install-extension ms-python.python --force  --extensions-dir vscode-extensions_dir
+code-server-$CODE_SERVER_VERSION-$sysx-amd64/bin/code-server --install-extension ms-python.python --force  --extensions-dir vscode-extensions_dir
