ca01d7200111ef742859c40d203148fe8830ce88
Ankan Kumar Bhunia <ankankumarbhunia@gmail.com>
2023-12-23 23:42:09 +0530

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -1,11 +1,14 @@
-import dash, subprocess, psutil, datetime
-import dash_html_components as html
-import requests, dash_table
-from bs4 import BeautifulSoup
+## requirements: nslookup
+
+from http.server import BaseHTTPRequestHandler, HTTPServer
 import pandas as pd
-import dash_bootstrap_components as dbc
-from dash import Dash, dcc, html, callback, Input, Output
-import lxml.html
+import subprocess, psutil, datetime
+import requests
+from bs4 import BeautifulSoup
+from flask import Flask
+
+SERVER_IP = '217.160.147.188'
+HOST_USERNAME = 'root'
 
 def get_time_info(timestamp):
 
@@ -37,109 +40,142 @@ def get_time_info(timestamp):
 
     return output
 
-def get_title(url):
-    try:
-        # response = requests.get(url)
-        # soup = BeautifulSoup(response.content, "html.parser")
-        # title = soup.find("title").text
-        t = lxml.html.parse(url)
-        return t.find(".//title").text
-    except:
-        return 'N/A'
+def get_dns_addr(ip_address):
+    dns = subprocess.getoutput("nslookup "+ip_address+" | awk '/name/ {print $4}'")
+    if dns == '':
+        return ip_address
+    else:
+        return dns
 
 def get_urls():
 
-    output = subprocess.getoutput("netstat -lnt | awk 'NR>2{print $4}' | grep -E '127.0.0.1:' | sed 's/.*://' | sort -n | uniq")
-
+    output = subprocess.getoutput("netstat -lntp | awk 'NR>2{print $4,$7}' |  grep -E '127.0.0.1:' | grep sshd | sort -n")
 
     if output == '':
-        dictionary={"Date":[], "SSH Command":[], "Hosts":[],"URLs":[]}
+        dictionary={"Date":[], "SSH Command":[], "Host": [], "URLs":[]}
         return pd.DataFrame(dictionary) 
-        
-    l = subprocess.getoutput("lsof -iTCP  | grep ' 10u ' | grep LISTEN | grep IPv4 | awk '{print $2,$9}'")
-    l2 = {i.split(' ')[0]:i.split(' ')[-1].split(':')[-1] for i in l.split('\n')}
-    h = subprocess.getoutput("lsof -iTCP  | grep ' 3u ' | grep ESTABLISHED | grep IPv4 | awk '{print $2,$9}'")
-    h2 = {i.split(' ')[0]:i.split('->')[-1].split(':')[0] for i in h.split('\n')}
-    g = {l2[i]:h2[i] for i in l2}
     
-    ports = [i for i in output.split('\n') if i not in ['22', '8000']]
+    port2pid = {i.split(' ')[0].split(':')[1]: i.split(' ')[1].split('/')[0] for i in output.split('\n')}
+    port2pid = {i:j for i,j in port2pid.items() if not (i in ['22', '8000'])}
+    port2pid = {i:j for i,j in port2pid.items() if int(i)<20000}
+    ports = [i for i in port2pid.keys() ]
+
+    server_ips = [subprocess.getoutput("netstat -lntpa | grep "+pid+" | grep ESTABLISHED | awk '{print $5}'") for pid in port2pid.values()]
+    server_ips = [[j.split(':')[0] for j in i.split('\n') if j.split(':')[0] not in ['0.0.0.0','127.0.0.1']][0] for i in server_ips]
+    server_ips = [get_dns_addr(ip_address) for ip_address in server_ips]
+
+    host_dict = {port_i:ip_i for ip_i, port_i in zip(server_ips, ports)}
+    
+
     datetimes  = [psutil.Process(int(subprocess.getoutput('fuser '+str(i)+'/tcp').split(' ')[-1])).create_time() for i in ports]
-    ports = [j for i,j in sorted(zip(datetimes,ports), reverse=True)]
-    hosts = [g[i] for i in ports]
+    ports = [j for i,j in sorted(zip(datetimes, ports), reverse=True)]
+    hosts = [host_dict[i] for i in ports]
 
     if output != '':    
         urls = ['http://localhost:'+i for i in ports]
     else:
         urls = []
 
-    #urls = [{'IP':'http://217.160.147.188:'+i, 'URL':'http://app.lonelycoder.live:'+i, 'Title':get_title('http://217.160.147.188:'+i)} for i in urls]
-
-    #title=[get_title(i) for i in urls]
-    sshs=['ssh -L '+i+':localhost:'+i+' -N root@217.160.147.188' for i in ports]
+    sshs=['autossh -M 20000 -f -N  -L '+i+':localhost:'+i+' -N '+HOST_USERNAME+'@'+SERVER_IP for i in ports]
     
-    ips=[html.A(html.P(i),href=i) for i in urls]
+    ips=[i for i in urls]
 
-    dictionary={"Date":[get_time_info(int(i))for i in sorted(datetimes, reverse=True)],"SSH Command":sshs, "Hosts":hosts, "URLs":ips}
+    dictionary={"Date":[get_time_info(int(i))for i in sorted(datetimes, reverse=True)],"SSH Command":sshs, "Host":hosts, "URLs":ips}
     df=pd.DataFrame(dictionary)
 
     return df
 
 def get_public_urls():
-    
-    output = subprocess.getoutput("netstat -lnt | awk 'NR>2{print $4}' | grep -E '0.0.0.0:' | sed 's/.*://' | sort -n | uniq")
+
+    output = subprocess.getoutput("netstat -lntp | awk 'NR>2{print $4,$7}' |  grep -E '0.0.0.0:' | grep sshd | sort -n")
+
     if output == '':
-        dictionary={"Date":[], "Hosts":[], "URLs":[]}
+        dictionary={"Date":[], "SSH Command":[], "Hosts":[],"URLs":[]}
         return pd.DataFrame(dictionary) 
-        
-    l = subprocess.getoutput("lsof -iTCP  | grep ' 10u ' | grep LISTEN | awk '{print $2,$9}'")
-    l2 = {i.split(' ')[0]:i.split(' ')[-1].split(':')[-1] for i in l.split('\n')}
-    h = subprocess.getoutput("lsof -iTCP  | grep ' 3u ' | grep ESTABLISHED | awk '{print $2,$9}'")
-    h2 = {i.split(' ')[0]:i.split('->')[-1].split(':')[0] for i in h.split('\n')}
-    g = {l2[i]:h2[i] for i in l2}
     
-    ports = [i for i in output.split('\n') if i not in ['22', '8000']]
+    port2pid = {i.split(' ')[0].split(':')[1]: i.split(' ')[1].split('/')[0] for i in output.split('\n')}
+    port2pid = {i:j for i,j in port2pid.items() if not (i in ['22', '8000'])}
+    port2pid = {i:j for i,j in port2pid.items() if int(i)<20000}
+
+    ports = [i for i in port2pid.keys()]
+
+    server_ips = [subprocess.getoutput("netstat -lntpa | grep "+pid+" | grep ESTABLISHED | awk '{print $5}'") for pid in port2pid.values()]
+    server_ips = [[j.split(':')[0] for j in i.split('\n') if j.split(':')[0] not in ['0.0.0.0','127.0.0.1']][0] for i in server_ips]
+    server_ips = [get_dns_addr(ip_address) for ip_address in server_ips]
+
+    host_dict = {port_i:ip_i for ip_i, port_i in zip(server_ips, ports)}
+    
+
     datetimes  = [psutil.Process(int(subprocess.getoutput('fuser '+str(i)+'/tcp').split(' ')[-1])).create_time() for i in ports]
-    ports = [j for i,j in sorted(zip(datetimes,ports), reverse=True)]
-    hosts = [g[i] for i in ports]
+    ports = [j for i,j in sorted(zip(datetimes, ports), reverse=True)]
+    hosts = [host_dict[i] for i in ports]
 
     if output != '':    
-        urls = ['http://217.160.147.188:'+i for i in ports]
+        urls = ['http://'+SERVER_IP+':'+i for i in ports]
     else:
         urls = []
 
-    ips=[html.A(html.P(i),href=i) for i in urls]
 
-    dictionary={"Date":[get_time_info(int(i))for i in sorted(datetimes, reverse=True)],"Hosts":hosts,  "URLs":ips}
+    sshs=['---' for i in ports]
+    
+    ips=[i for i in urls]
+
+    dictionary={"Date":[get_time_info(int(i))for i in sorted(datetimes, reverse=True)], "SSH Command":sshs, "Host":hosts,"URLs":ips}
     df=pd.DataFrame(dictionary)
 
     return df
 
-app = dash.Dash(external_stylesheets=[dbc.themes.CYBORG])
-app.title = 'Dashboard'
-app.layout = html.Div([html.Div(style = {'width': '70%'}, id='main'
-), html.Br(),html.Button('Refresh', id='trigger', style={'display':'none'})])
+def get_html_():
 
-@callback(
-    Output('main', 'children'),
-    [Input('trigger', 'n_clicks')])
-def display_page(relative_pathname):
-    return html.Div(
-    [
-        dbc.Modal(
-            [
-                dbc.ModalHeader(dbc.ModalTitle("Private Tunnels"), close_button=False),
-                dbc.ModalBody(dbc.Table.from_dataframe(get_urls(), striped=True, bordered=True, hover=True)),
-                dbc.ModalHeader(dbc.ModalTitle("Public Tunnels"), close_button=False),
-                dbc.ModalBody(dbc.Table.from_dataframe(get_public_urls(), striped=True, bordered=True, hover=True)),
-            ],
-            id="modal-centered",
-            centered=True,
-            is_open=True,
-            size="xl",
-        ),
-        
-    ]
-)
+    html_code =  pd.merge(get_urls(), get_public_urls(), how='outer').to_html(index=False, escape=False, render_links=True)
+
+    # HTML template with centering and spaces
+    html_template = f"""
+    <!DOCTYPE html>
+    <html>
+    <head>
+        <style>
+            body {{
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                height: 100vh;
+                margin: 0;
+                background-color: #1E1E1E;  /* Dark background color */
+                color: #FFFFFF;  /* Light text color */
+            }}
+            table {{
+                border-collapse: collapse;
+                width: 80%;
+                margin-top: 20px;
+                margin-bottom: 20px;
+                background-color: #2D2D2D;  /* Dark table background color */
+            }}
+            th, td {{
+                border: 1px solid #3E3E3E;  /* Dark border color */
+                padding: 8px;
+                text-align: left;
+                color: #FFFFFF;  /* Light text color */
+            }}
+            a {{
+                color: #3498DB;  /* Blue link color */
+                text-decoration: underline;
+            }}
+        </style>
+    </head>
+    <body>
+        {html_code}
+    </body>
+    </html>
+    """
+    return html_template
+
+kk =  get_urls()
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return get_html_()
 
 if __name__ == '__main__':
-    app.run_server('0.0.0.0', 8000)
+    app.run('0.0.0.0', 8000)
